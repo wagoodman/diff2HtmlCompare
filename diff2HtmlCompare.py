c453d00e7fef1afcd301e10fca01334e23af4360
AlexKutas <kutasandsasha@gmail.com>
2022-10-05 12:24:29 +0300

    Update diff2HtmlCompare.py

--- diff2HtmlCompare.py
+++ diff2HtmlCompare.py
@@ -145,7 +145,7 @@ class DiffHtmlFormatter(HtmlFormatter):
         self.diffs = diffs
         super(DiffHtmlFormatter, self).__init__(*args, **kwargs)
 
-    def wrap(self, source, outfile):
+    def wrap(self, source):
         return self._wrap_code(source)
 
     def getDiffLineNos(self):
